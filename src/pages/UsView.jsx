9c53a257efb52e7d5771398932a0cbc994d7206f
Christopher Sosa <urri_@hotmail.com>
2022-06-09 18:15:41 -0500

    Cambios generales en el componente nosotros

--- src/pages/UsView.jsx
+++ src/pages/UsView.jsx
@@ -1,10 +1,140 @@
 import { NavBar } from "../components/navbar/NavBar";
+import { Footer } from "../components/footer/Footer";
 
 export function UsView() {
   return (
-    <div>
-      <NavBar />
-      <h1>Desde UsView</h1>
-    </div>
+    <>
+      <NavBar transparent />
+      <main>
+        <div className="relative pt-16 pb-32 flex content-center items-center justify-center">
+          <div
+            className="absolute top-0 w-full h-full bg-center bg-cover"
+            style={{
+              backgroundImage:
+                "url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80')",
+            }}
+          >
+            <span
+              id="blackOverlay"
+              className="w-full h-full absolute opacity-75 bg-black"
+            ></span>
+          </div>
+          <div className="container header relative pt-16 items-center flex h-screen max-h-860-px">
+            <div className="items-center flex flex-wrap">
+              <div className="w-full md:w-8/12 lg:w-6/12 xl:w-6/12 px-4">
+                <div className="pr-12">
+                  <h1 className="text-white font-semibold text-5xl">
+                    Your story starts with us.
+                  </h1>
+                  <p className="mt-4 text-lg text-blue-200">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book. It has survived not only five centuries,
+                    but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised in the 1960s with
+                    the release of Letraset sheets containing Lorem Ipsum
+                    passages, and more recently with desktop publishing software
+                    like Aldus PageMaker including versions of Lorem Ipsum.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+            style={{ transform: "translateZ(0)" }}
+          >
+            <svg
+              className="absolute bottom-0 overflow-hidden"
+              xmlns="http://www.w3.org/2000/svg"
+              preserveAspectRatio="none"
+              version="1.1"
+              viewBox="0 0 2560 100"
+              x="0"
+              y="0"
+            ></svg>
+          </div>
+        </div>
+        <section className="pt-20 pb-48">
+          <div className="container mx-auto px-4">
+            <div className="flex flex-wrap justify-center text-center mb-24">
+              <div className="w-full lg:w-6/12 px-4">
+                <h2 className="text-4xl font-semibold">Here are our heroes</h2>
+                <p className="text-lg leading-relaxed m-4 text-blue-500">
+                  According to the National Oceanic and Atmospheric
+                  Administration, Ted, Scambos, NSIDClead scentist, puts the
+                  potentially record maximum.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    // src falta poner fotos
+                    className="shadow-lg rounded-full mx-auto max-w-120-px"
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">Ryan Tompson</h5>
+                    <p className="mt-1 text-sm text-blue-400 uppercase font-semibold">
+                      Web Developer
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    // src falta poner fotos
+                    className="shadow-lg rounded-full mx-auto max-w-120-px"
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">Romina Hadid</h5>
+                    <p className="mt-1 text-sm text-blue-400 uppercase font-semibold">
+                      Marketing Specialist
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    // src falta poner fotos
+                    className="shadow-lg rounded-full mx-auto max-w-120-px"
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">Alexa Smith</h5>
+                    <p className="mt-1 text-sm text-blue-400 uppercase font-semibold">
+                      UI/UX Designer
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
+                <div className="px-6">
+                  <img
+                    alt="..."
+                    // src falta poner fotos
+                    className="shadow-lg rounded-full mx-auto max-w-120-px"
+                  />
+                  <div className="pt-6 text-center">
+                    <h5 className="text-xl font-bold">Jenna Kardi</h5>
+                    <p className="mt-1 text-sm text-blue-400 uppercase font-semibold">
+                      Founder and CEO
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
   );
 }
