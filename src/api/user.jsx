d54e939a37d786bf08ddb1434124bedd2ac82684
Christopher <urri_@hotmail.com>
2022-06-13 20:05:51 -0500

    Campos agregar al sistema

---
+++ src/api/user.jsx
@@ -0,0 +1,65 @@
+import { API_URL } from "../utils/constants";
+import { authFetch } from "../utils/fetch";
+export async function registerApi(formData) {
+  try {
+    const url = `${API_URL}/auth/local/register`;
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export async function loginApi(formData) {
+  try {
+    const url = `${API_URL}/auth/local`;
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function resetPasswordApi(email) {
+  try {
+    const url = `${API_URL}/auth/forgot-password`;
+    const params = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    };
+    const response = await fetch(url, params);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getMeApi(logout) {
+  try {
+    const url = `${API_URL}/users/me`;
+    const result = await authFetch(url, null, logout);
+    return result ? result : null;
+  } catch (error) {
+    return null;
+  }
+}
