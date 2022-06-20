1df579162123e596fdedec393a1904453893377d
Christopher <urri_@hotmail.com>
2022-06-20 10:06:20 -0500

    Cambios en navbar

--- src/components/navbar/NavBarDashboard.jsx
+++ src/components/navbar/NavBarDashboard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
-import { getMeApi } from "../../api/user";
+import { getMeApi } from "../../api/admin/user";
 
 export function NavBarDashboard() {
   const [user, setUser] = useState(undefined);
@@ -9,27 +10,30 @@ export function NavBarDashboard() {
     (async () => {
       const response = await getMeApi(logout);
       setUser(response);
+      console.log("Desde navnardas" + response);
     })();
-  }, [auth, logout]);
+  }, [auth]);
   return (
     <>
-      <div class="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
-        <div class="sticky top-0 h-16 border-b bg-white lg:py-2.5">
-          <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
-            <h5 hidden class="text-2xl text-gray-600 font-medium lg:block">
+      <div className="ml-auto mb-6 ">
+        <div className="sticky top-0 h-16 border-b bg-white lg:py-2.5">
+          <div className="px-6 flex items-center justify-between space-x-4 2xl:container">
+            <h5 hidden className="text-2xl text-gray-600 font-medium lg:block">
               Bienvenido {user?.username}
             </h5>
-            <button class="w-12 h-16 -mr-2  lg:hidden"></button>
-            <div class="flex space-x-4">
-              <img
-                src={
-                  user?.avatar
-                    ? user?.avatar
-                    : "https://via.placeholder.com/150"
-                }
-                alt="logo"
-                class="h-10 w-10 rounded-full"
-              />
+            <button className="w-12 h-16 -mr-2  lg:hidden"></button>
+            <div className="flex space-x-4">
+              <Link to="/admin/usuario">
+                <img
+                  src={
+                    user?.url
+                      ? user?.url
+                      : "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
+                  }
+                  alt="logo"
+                  className="h-10 w-10 rounded-full"
+                />
+              </Link>
             </div>
           </div>
         </div>
