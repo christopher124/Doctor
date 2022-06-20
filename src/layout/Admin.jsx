d2fd3bfd22956f6dfdfd19c8e230116384f8034b
Christopher <urri_@hotmail.com>
2022-06-20 10:08:57 -0500

    Cambios generales y creacion de nuevos modulos

--- src/layout/Admin.jsx
+++ src/layout/Admin.jsx
@@ -1,20 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
 
-// components
-
-import Sidebar from "../components/sidebar/SideBar";
-
-// views
-
-export default function Admin() {
-  return (
-    <>
-      <Sidebar />
-      <div className="relative md:ml-64 bg-blueGray-100">
-        {/* Header */}
-        <div className="px-4 md:px-10 mx-auto w-full -m-24"></div>
-      </div>
-    </>
-  );
+export function Admin() {
+  return <div>Admin</div>;
 }
