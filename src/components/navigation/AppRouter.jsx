cd03cee6d1ac6b619a84e38f11a939024492d6e8
Christopher <urri_@hotmail.com>
2022-06-20 10:06:46 -0500

    Cambios en la estructura de rutas

--- src/components/navigation/AppRouter.jsx
+++ src/components/navigation/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import { ContactView } from "../../pages/ContactView";
-import { DashboardView } from "../../pages/dashboard/DashboardView";
+import { DashboardView } from "../../pages/admin/dashboard/DashboardView";
 import { HomeView } from "../../pages/HomeView";
 import { LoginView } from "../../pages/LoginView";
 import { NotFound } from "../../pages/NotFound";
 import { QuotesView } from "../../pages/QuotesView";
 import { ServicesView } from "../../pages/ServicesView";
 import { UsView } from "../../pages/UsView";
-
+import { ClientsView } from "../../pages/admin/client/ClientsView";
+import { SideBar } from "../sidebar/SideBar";
+import { UserView } from "../../pages/admin/dashboard/UserView";
 export function AppRouter() {
   return (
     <div>
@@ -21,8 +23,13 @@ export function AppRouter() {
         <Route path="/login" element={<LoginView />} />
         <Route path="*" element={<NotFound />} />
         {/* Rutas privadas: */}
-        <Route path="/dashboard" element={<DashboardView />} />
+        <Route path="/admin" element={<SideBar />}>
+          <Route path="dashboard" element={<DashboardView />} />
+          <Route path="clientes" element={<ClientsView />} />
+          <Route path="usuario" element={<UserView />} />
+        </Route>
       </Routes>
+      {/* Rutas privadas: */}
     </div>
   );
 }
