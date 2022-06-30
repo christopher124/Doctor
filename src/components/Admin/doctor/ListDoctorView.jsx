f417576e7848e60456cea1371a0818214fb12a28
Christopher <urri_@hotmail.com>
2022-06-30 18:50:10 -0500

    Cambios en la vista doctores

--- src/components/Admin/doctor/ListDoctorView.jsx
+++ src/components/Admin/doctor/ListDoctorView.jsx
@@ -39,10 +39,10 @@ export function ListDoctorView({ doctor, handleDelited }) {
 
   return (
     <>
-      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <tr className="bg-white border-b bg-gray-800 border-gray-700">
         <th
           scope="row"
-          className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+          className="px-6 py-4 font-medium text-gray-900 text-white whitespace-nowrap"
         >
           {name} {last}
         </th>
