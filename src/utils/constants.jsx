f36150c9ca976c77ae81724ca60cb7b0bb9e9052
Christopher Sosa <urri_@hotmail.com>
2023-03-05 00:20:25 -0600

    Cambio de puerto en dev

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL_DEV = "http://localhost:1337";
+export const API_URL_DEV = "http://localhost:3002";
 export const API_URL_PROD = "https://server--doctor.herokuapp.com";
 export const TOKEN = "token";
 export const TOKENMAP = "AIzaSyCDNosWdTkjQXCLGX4A-ZhS5MI0nCEnSYw";
